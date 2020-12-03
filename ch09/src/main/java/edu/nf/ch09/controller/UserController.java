0fb0c8ffcd4bce34fb67aa65d01b47c1118254d0
QGL <2746343370@qq.com>
2020-12-03 15:44:52 +0800

    commit project

--- ch09/src/main/java/edu/nf/ch09/controller/UserController.java
+++ ch09/src/main/java/edu/nf/ch09/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
+import javax.annotation.Resource;
+
 /**
  * @author 0.0
  * @date 2020/12/3
@@ -28,6 +30,11 @@ public class UserController {
     @Autowired
     //通过@Qualifier强制指定Bean的id
     //@Qualifier("stuService")
+
+    //也可以使用Resource注解来注入，它是javax包下提供的注解
+    //Spring可以支持此注解，name指定要注入的Bean的id
+    //只能标注在字段以及set方法上，不能标注在构造方法上
+    @Resource(name = "stuService")
     private UserService Service;
 
 
@@ -42,10 +49,12 @@ public class UserController {
 
     /**
      * 注入Service对象
-     * @param userService
+     * @param //userService
      * @Autowired注解用于完成注入配置，可以标识在set方法上，构造方法上，字段上。
      * 注意：当实现类只要一个的时候，Spring会自动找到它并直接注入进来
-     * 如果有多个实现类的时候就必须结合@Qualifier注解一起使用，
+     * 如果有多个实现类的时候,先根据变量的名称作为id从容器中查找
+     * 如果匹配的到，则直接返回，匹配不到则抛出有多个实现类的异常
+     * 就必须结合@Qualifier注解一起使用，
      * 这个注解的作用是指定要注入哪一个实现类
      */
    /* @Autowired
